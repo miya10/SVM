80e0f582a82edaf3a0d10d3b64ecede82094bdbb
miya10 <miya00223@gmail.com>
2019-10-25 14:31:46 +0900

    add

--- SVR/svr.py
+++ SVR/svr.py
@@ -5,9 +5,8 @@ import init, svm
 from kernel import *
 
 # SVR実装モデル
-def fit(x, y, kernel):
+def fit(x, y, kernel, C=1000.0):
     eps = 0.1
-    C = 1000.0
     # cvxoptの形式にデータを変換
     m, n = x.shape
     y = y.reshape(-1, 1)
@@ -21,21 +20,21 @@ def fit(x, y, kernel):
             else:
                 P[i][j] = eval(kernel)(x[i], x[j])
             P[m + i][m + j] = P[i][j]
-            P[i + m][j] = -1.0 * P[i][j]
-            P[i][m + j] = -1.0 * P[i][j]
+            P[i + m][j] = - 1.0 * P[i][j]
+            P[i][m + j] = - 1.0 * P[i][j]
     P = matrix(P)
     q = np.zeros(m * 2)
     for i in range(m):
         q[i] = 1.0 * y[i] + eps
         q[i + m] = -1.0 * y[i] + eps
     q = matrix(q)
-    tmp1 = -1.0 * np.diag(np.ones(m * 2))
+    tmp1 = - 1.0 * np.diag(np.ones(m * 2))
     tmp2 = np.diag(np.ones(m * 2))
     G = matrix(np.vstack((tmp1, tmp2)))
     tmp1 = np.zeros(m * 2)
     tmp2 = np.ones(m * 2) * C
     h = matrix(np.hstack((tmp1, tmp2)))
-    A = matrix(np.append(np.ones(m), -1.0*np.ones(m)), (1, m*2))
+    A = matrix(np.append(np.ones(m), - 1.0 * np.ones(m)), (1, m * 2))
     b = matrix(np.zeros(1))
 
     # solverのパラメータの調整 
@@ -47,23 +46,52 @@ def fit(x, y, kernel):
     # solverを実行
     sol = solvers.qp(P, q, G, h, A, b)
     alphas = np.array(sol['x'])
-    w = (alphas[:m] - alphas[m:]).ravel()
-    """
-    b_array = y - _inner_mat(self.x) + eps
-    self._b = np.mean(b_array[self.support_vector_id])"""
-    print(alphas)
-    print(w.T)
-    print(x[0])
+    alphas = np.where(alphas < 1e-6, 0.0, alphas)
+    S = (alphas > 1e-6).flatten()
+    diff = (alphas[m:] - alphas[:m]).ravel()
+    support_vector = np.arange(m)[(diff > eps) | (diff < -eps)]
+    w = np.zeros(x.shape[1])
+    for i in range(len(x)):
+        tmp_w = (alphas[m+i] - alphas[i]) * x[i]
+        w += tmp_w
 
-    f = np.dot(w.T,x[0])
-    print(f)
-    return alphas, w
+    b = np.zeros(len(x))
+    for i in range(len(x)):
+        tmp_b_sum = 0
+        for j in range(len(x)):
+            tmp_b = diff[j] * np.dot(x[i], x[j])
+            if kernel == None:
+                tmp_b = diff[j] * np.dot(x[i], x[j])
+            else:
+                tmp_b = diff[j] * eval(kernel)(x[i], x[j])
+            tmp_b_sum += tmp_b
+        if diff[i] > 0:
+            b[i] = - y[i] + tmp_b_sum + eps
+        else:
+            b[i] = - y[i] + tmp_b_sum - eps
+    b = np.average(b[support_vector])
+    return alphas, w, b
+
+def predict(x, w, b, kernel):
+    result = np.zeros(len(x))
+    if kernel == None:
+        for i in range(len(x)):
+            result[i] = np.dot(w, x[i]) - b
+    else:
+        for i in range(len(x)):
+            result[i] = eval(kernel)(w, x[i]) - b
+    return result
 
 # メイン関数
 def main():
     filename, kernel, n = init.set_parser()
     x, y = init.load_data(filename)
-    alphas, w = fit(x, y, kernel)
+    alphas, w, b = fit(x, y, kernel)
+    result = predict(x, w, b, kernel)
+    print('alpha：',alphas.flatten())
+    print('重み：', w)
+    print('閾値：', b)
+    print('result = ', result)
 
 if __name__ == '__main__':
     main()
---
+++ SVR/svr_cross_validation.py
@@ -0,0 +1,32 @@
+import sys
+sys.path.append('../SVM')
+from init import *
+from cross_validation import split_data
+import svr
+
+def cross_validate(train_x, test_x, train_y, test_y, kernel, C):
+    alphas, w, b = svr.fit(train_x, train_y, kernel, C)
+    predict_arr = svr.predict(test_x, w, b, kernel)
+    mse = 0
+    for i in range(len(test_y)):
+        tmp = (test_y[i] - predict_arr[i]) ** 2
+        mse += tmp
+    return mse / len(test_y)
+
+def main():
+    filename, kernel, n = set_parser()
+    x, y = load_data(filename)
+    c_list = [1.0, 10.0, 100.0, 1000.0]
+    for j in range(len(c_list)):
+        print('---- C = '+str(c_list[j])+' ----')
+        total_mse = 0
+        for i in range(n):
+            train_x, test_x, train_y, test_y = split_data(x, y, n, i)
+            mse = cross_validate(train_x, test_x, train_y, test_y, kernel, c_list[j])
+            print('MSE = ', mse)
+            total_mse += mse
+        total_mse = total_mse / n
+        print('評価結果：', total_mse)
+
+if __name__ == '__main__':
+    main()
